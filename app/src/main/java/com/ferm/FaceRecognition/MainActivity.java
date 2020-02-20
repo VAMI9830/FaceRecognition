31ac6df859bdd28ab4b32b87d4726a936fd278bd
VAMI9830 <11700224@student.pxl.be>
2020-02-20 16:56:16 +0100

    added functionality to some buttons, tried some things for face detection without success.

--- app/src/main/java/com/ferm/FaceRecognition/MainActivity.java
+++ app/src/main/java/com/ferm/FaceRecognition/MainActivity.java
@@ -2,15 +2,21 @@ package com.ferm.FaceRecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private MaterialButton button;
 
     private DatabaseReference mDatabase;
 
@@ -23,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        button = (MaterialButton)findViewById(R.id.sign_in_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(true) {
+                    startActivity(new Intent(MainActivity.this, FaceCapture.class));
+                }
+                else {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT);
+                    toast.setMargin(50,50);
+                    toast.show();
+                    finishActivity(0);
+                }
+            }
+            });
+
     }
 
     public void writeNewKid(String collection, String kidId, String voornaam, String naam) {
