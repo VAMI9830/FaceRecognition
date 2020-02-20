56b0f6b301595f98c55b89e48f05da5676be58c0
JeroenCops <jernncops@live.be>
2020-02-20 14:53:32 +0100

    Reading and writing to firebase

--- app/src/main/java/com/ferm/FaceRecognition/MainActivity.java
+++ app/src/main/java/com/ferm/FaceRecognition/MainActivity.java
@@ -1,34 +1,69 @@
 package com.ferm.FaceRecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private DatabaseReference mDatabase;
+    private FirebaseFirestore mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        this.mDatabase = FirebaseFirestore.getInstance();
 
-        myRef.setValue("Hello, World!");
+        this.writeNewKid("Inschrijvingen", "1", "Karate", "Kid");
     }
 
     public void writeNewKid(String collection, String kidId, String voornaam, String naam) {
         Kid kid = new Kid(voornaam, naam);
 
         Log.v(TAG, "Adding a kid to database");
-        mDatabase.child(collection).child(kidId).setValue(kid);
+        mDatabase.collection(collection)
+                .add(kid)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        mDatabase.collection("Inschrijvingen")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
